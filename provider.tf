d4258197fbd300d63b2d8f82fa14287bd033927d
sainaveen143 <109063142+sainaveen143@users.noreply.github.com>
2023-10-02 17:10:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
 
-provider "aws" {
-    access_key = "AKIAY2OU7OZJ57G7X3EJ"
-    secret_key = "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"
-}
